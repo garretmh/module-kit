a9dea21f3b3890e98979f2d65f8000cdedf70404
Garret <github@garretmh.com>
2016-12-12 22:42:24 -0600

    Add the module classes back in

---
+++ init.php
@@ -0,0 +1,91 @@
+<?php
+/*
+Plugin Name: Module Kit
+Description: A WordPress Plugin for projects to combine disparate functions
+Version: 0.9.0
+Author: Garret McGraw-Hanson
+Author URI: http://garretmh.github.com
+*/
+
+if ( ! class_exists( 'MK_Bootstrap', false ) ) {
+	/**
+	 * Handles checking for and loading the Module Kit and its dependencies
+	 */
+	class MK_Bootstrap {
+		/**
+		 * The name of this class
+		 *
+		 * @var string
+		 */
+		public static $name = __CLASS__;
+
+		/**
+		 * Single instance of the MK_Bootstrap object
+		 *
+		 * @var MK_Bootstrap
+		 */
+		public static $single_instance = null;
+
+		/**
+		 * Creates/returns the single instance MK_Bootstrap object
+		 *
+		 * @return MK_Bootstrap Single instance object
+		 */
+		public static function initiate() {
+			if ( null === self::$single_instance ) {
+				self::$single_instance = new self();
+			}
+			return self::$single_instance;
+		}
+
+		/**
+		 * Start the plugin
+		 *
+		 * Sets up globals and plugin hooks
+		 */
+		private function __construct() {
+			# Globally define the plugin directory path
+			$dirname = __CLASS__ . '_DIR';
+			if ( ! defined( $dirname ) ) {
+				define( $dirname, trailingslashit( dirname( __FILE__ ) ) );
+			}
+
+			# Setup hooks & shortcodes
+			add_action( 'init', [__CLASS__, 'bootstrap'], 20 );
+
+			# Initilize
+			require_once 'inc/CMB2/init.php';
+			require_once 'inc/class-template-loader.php';
+			require_once 'inc/class-options.php';
+			require_once 'inc/class-module.php';
+			require_once 'module.php';
+			# Initialization Complete.
+		}
+
+		/**
+		 * Module Kit bootstrap proccess
+		 */
+		public static function bootstrap() {
+			if ( is_admin() ) {
+				/**
+				 * Fires on the admin side when the Module Kit is included/loaded.
+				 */
+				do_action( 'mk_admin_init' );
+			}
+
+			/**
+			 * Fires when the Module Kit is included/loaded
+			 *
+			 * Can be used to add modules
+			 */
+			do_action( 'mk_init' );
+
+			/**
+			 * Fires after the Module Kit initiation process has been completed
+			 */
+			do_action( 'mk_after_init' );
+		}
+	} # End class.
+}
+
+MK_Bootstrap::initiate();
---
+++ module.php
@@ -0,0 +1,126 @@
+<?php
+if ( ! class_exists( 'MK_Module', false ) ) {
+	/**
+	 * MK Module Kit
+	 *
+	 * Template module for building actual Module Kit modules
+	 *
+	 * @author   Garret McGraw-Hanson
+	 * @version  0.10.0
+	 *
+	 * @property bool   $is_root                      overrideable  Whether the module is a Root Module or Submodule
+	 * @property string $key                                        Identification key of the module (ie mk_example)
+	 * @property object $options_page                               MK_Module_Kit_Options object
+	 * @property string $path                                       Path to this class from plugin root
+	 * @property string $prefix                       overrideable  Module naming prefix
+	 * @property string $title                        overrideable  Module display title (ie MK Example)
+	 *
+	 * @static   object get_instance()                              Returns instance(s) of Module_Kit objects
+	 * @static   object initiate()                                  Creates/returns an instance of the particular Module_Kit object
+	 *
+	 * @method   void   activate()                    overrideable  What to do when the module runs (ie extends initiate())
+	 * @method   bool   add_action()                                Hooks a function on to a specific action if the module is enabled.
+	 * @method   bool   add_filter()                                Hook a function or method to a specific filter action if the module is enabled.
+	 * @method   void   add_options_fields( object )  overrideable  Register the module's options page metabox
+	 * @method   void   add_options_page( array )                   Create/Get a module options page
+	 * @method   mixed  get_option( string )                        Get CMB2 options from Module's options page
+	 * @method   object get_parent()                                Retrieve the parent module object if one exists.
+	 * @method   object get_root()                                  Retrieve the root module object.
+	 * @method   object get_template_loader()                       Get/create a template loader for the module
+	 * @method   string get_template_part( string, string, bool )   Retrieve a template part
+	 * @method   bool   is_enabled()                                Check whether this module is active
+	 * @method   void   load_submodules()                           Load any submodules in the mod directory once.
+	 * @method   string locate_template( mixed, bool, bool )        Retrieve the name of the highest priority template file that exists
+	 * @method   object parent()                                    Alias for the get_parent method
+	 */
+	class MK_Module extends Module_Kit {
+
+		/**
+		 * Module title
+		 * @internal Overrideable
+		 *           Default: ''
+		 *           Defaults to the class name
+		 * @var string
+		 */
+		protected $title = '';
+
+		/**
+		 * Module naming prefix
+		 * @internal Overrideable
+		 *           Default: 'mk'
+		 * @var string
+		 */
+		protected $prefix = 'mk';
+
+		/**
+		 * Whether the module is a Root Module or Submodule
+		 * @internal Overrideable
+		 *           Default: null
+		 *           If true, module will initiate as a root module
+		 * @var boolean
+		 */
+		private $is_root = null;
+
+		/**
+		 * Run the module
+		 *
+		 * Add hooks, register an options page, load submodules
+		 * @internal Overrideable
+		 */
+		protected function activate() {
+			Module_Kit::activate(); // default
+
+			# Register our options page
+			//$this->add_options_page();
+
+			# Setup up our hooks to always run
+			//add_action( 'init', [$this, 'init'] );
+			//add_filter( 'the_content', [$this, 'the_content'] );
+
+			# Set up our hooks to run when enabled
+			//$this->add_action( 'cmb2_admin_init', [$this, 'admin_init'] );
+			//$this->add_filter( 'the_content', [$this, 'the_content'] );
+
+			# Load our submodules
+			//$this->load_submodules();
+
+			# Set up our submodule-dependant hooks
+			// if ( static::class == self::class ) {
+			// 	# Setup up our mk_example related hooks
+			// 	if ( $submodule = self::get_instance('mk_example') ) {
+			// 		$submodule->add_filter( 'the_content', [$this, 'the_content'] );
+			// 	}
+			// }
+		}
+
+		/**
+		 * Register a metabox for the Module's options page
+		 * @internal Overrideable
+		 * @see  Module_Kit::add_options_page
+		 * @uses CMB2
+		 */
+		public function add_options_fields( $cmb ) {
+			# Create a checkbox for each submodule
+			Module_Kit::add_options_fields($cmb); // default
+
+			# Set our CMB2 fields
+			// $cmb->add_field([
+			// 	'name' => __( 'Test Text', 'myprefix' ),
+			// 	'desc' => __( 'field description (optional)', 'myprefix' ),
+			// 	'id'   => 'test_text',
+			// 	'type' => 'text',
+			// 	'default' => 'Default Text',
+			// ]);
+			//
+			// $cmb->add_field([
+			//   'name'    => __( 'Test Color Picker', 'myprefix' ),
+			//   'desc'    => __( 'field description (optional)', 'myprefix' ),
+			//   'id'      => 'test_colorpicker',
+			//   'type'    => 'colorpicker',
+			//   'default' => '#bada55',
+			// ]);
+		}
+	} # End class.
+
+	MK_Module::initiate();
+}
